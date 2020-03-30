66674213029b2d1e90dc29a7d856711a53bde767
yamaimo <yarnaimo@gmail.com>
2020-03-30 17:44:14 +0900

    fix

--- init-config.sh
+++ init-config.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-CONFIG_PATH=src/.config/default.ts
+CONFIG_DIR=src/.config
+CONFIG_PATH=$CONFIG_DIR/default.ts
 
 if [ ! -f $CONFIG_PATH ]; then
+    mkdir -p $CONFIG_DIR
+
     echo "import { Config } from '../services/config'
 
 export const configDefault: Config = {
