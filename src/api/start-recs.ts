9ed2262b3309101d54ef1da4f1fbfbb5cfc06ab6
yamaimo <yarnaimo@gmail.com>
2020-05-17 21:47:20 +0900

    audioOnlyの録画後にvideoを削除する

--- src/api/start-recs.ts
+++ src/api/start-recs.ts
@@ -1,4 +1,5 @@
-import { $ } from '@yarnaimo/rain'
+import { unlink } from 'fs-extra'
+import { Do, MapAsync, P } from 'lifts'
 import { basename } from 'path'
 import { rec } from '../services/ag'
 import { appConfig } from '../services/config'
@@ -16,27 +17,37 @@ export const startRecs = async (currentDate: Dayjs) => {
 
     const dateStr = nextMinute.format('YYYYMMDD')
 
-    await $.p(
+    await P(
         reserves,
 
-        $.map(async ({ audioOnly, label, durationSeconds }) => {
+        MapAsync(async ({ audioOnly, label, durationSeconds }) => {
             const base = `${label}-${dateStr}`
             const videoPath = `.data/${base}.flv`
             const audioPath = `.data/${base}.aac`
 
             try {
                 await rec(durationSeconds, videoPath)
-                if (audioOnly) {
-                    await extractAudio(videoPath, audioPath)
-                }
-                const encPath = audioOnly ? audioPath : videoPath
-                const encFilename = basename(encPath)
+
+                const encodedPath = await Do(async () => {
+                    if (audioOnly) {
+                        await extractAudio(videoPath, audioPath)
+                        await unlink(videoPath)
+                        return audioPath
+                    } else {
+                        return videoPath
+                    }
+                })
+                const encodedFilename = basename(encodedPath)
 
                 if (config.driveFolder) {
-                    await uploadToDrive(encPath, config.driveFolder)
-                    await sendWebhook(`${encFilename} をアップロードしました`)
+                    await uploadToDrive(encodedPath, config.driveFolder)
+                    await sendWebhook(
+                        `${encodedFilename} をアップロードしました`,
+                    )
                 } else {
-                    await sendWebhook(`${encFilename} をローカルに保存しました`)
+                    await sendWebhook(
+                        `${encodedFilename} をローカルに保存しました`,
+                    )
                 }
             } catch (error) {
                 log(error)
